9c4966882c5605504fc99918830635a1a343c041
JRafaelMorantes <136393666+JRafaelMorantes@users.noreply.github.com>
2023-06-14 22:59:48 -0500

    Se Agrego Nave Enemiga

---
+++ game/components/enemies/enemy.py
@@ -0,0 +1,18 @@
+import random
+
+class Enemy:
+    
+    X_POS_LIST = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500]
+    Y_POS = 20
+    
+    def __init__(self, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = random.choice(self.X_POS_LIST)
+        self.rect.y = self.Y_POS
+    
+    def update(self):
+        pass
+    
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
