bbe0bbfef2cf9beef8d3449abb3e944f2f0038a3
JRafaelMorantes <136393666+JRafaelMorantes@users.noreply.github.com>
2023-06-15 14:15:22 -0500

    Tarea 2: Segunda Nave Enemiga

---
+++ game/components/enemies/alien_ship.py
@@ -0,0 +1,13 @@
+import pygame
+from game.components.enemies.enemy import Enemy
+from game.utils.constants import ENEMY_2
+
+class AlienShip(Enemy):
+    
+    WIDTH = 60
+    HEIGHT = 80
+    
+    def __init__(self):
+        self.image = ENEMY_2
+        self.image = pygame.transform.scale(self.image, (self.WIDTH, self.HEIGHT))
+        super().__init__(self.image)
--- game/components/enemies/enemy_handler.py
+++ game/components/enemies/enemy_handler.py
@@ -1,9 +1,11 @@
 from game.components.enemies.ship import Ship
+from game.components.enemies.alien_ship import AlienShip
 
 class EnemyHandler:
     def __init__(self):
         self.enemies = []
         self.enemies.append(Ship())
+        self.enemies.append(AlienShip())
     
     def update(self):
         for enemy in self.enemies:
