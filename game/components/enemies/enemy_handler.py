776d5ded84df3520f4fb187d7467ef1ac715d7d5
JRafaelMorantes <136393666+JRafaelMorantes@users.noreply.github.com>
2023-06-15 14:20:07 -0500

    modificacion tarea 2: una 3ra nave enemiga

--- game/components/enemies/enemy_handler.py
+++ game/components/enemies/enemy_handler.py
@@ -1,11 +1,13 @@
 from game.components.enemies.ship import Ship
 from game.components.enemies.alien_ship import AlienShip
+from game.components.enemies.alien_warship import AlienWarShip
 
 class EnemyHandler:
     def __init__(self):
         self.enemies = []
         self.enemies.append(Ship())
         self.enemies.append(AlienShip())
+        self.enemies.append(AlienWarShip())
     
     def update(self):
         for enemy in self.enemies:
