7ffed94f479633b7395e474c0fe204376db4b793
JRafaelMorantes <136393666+JRafaelMorantes@users.noreply.github.com>
2023-06-19 10:22:59 -0500

    modificaciones trabajo final

--- game/components/bullets/bullet_enemy.py
+++ game/components/bullets/bullet_enemy.py
@@ -1,14 +1,14 @@
 import pygame
 from game.components.bullets.bullet import Bullet
-from game.utils.constants import BULLET_ENEMY_2 ,BULLET_ENEMY_TYPE, SCREEN_HEIGHT, SHIELD_TYPE
+from game.utils.constants import BULLET_ENEMY ,BULLET_ENEMY_TYPE, SCREEN_HEIGHT, SHIELD_TYPE
 
 class BulletEnemy(Bullet):
-    WIDTH = 9 
+    WIDTH = 17 
     HEIGTH = 32
     SPEED = 20
 
     def __init__(self, center):
-        self.image = BULLET_ENEMY_2
+        self.image = BULLET_ENEMY
         self.image = pygame.transform.scale(self.image, (self.WIDTH, self.HEIGTH))
         self.type = BULLET_ENEMY_TYPE
         super().__init__(self.image,self.type, center)
